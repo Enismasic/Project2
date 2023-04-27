b78f9127067cb13596cf545665032baa5edb7d00
Enismasic <emasic@hawk.iit.edu>
2023-04-27 01:48:31 -0500

    Change JS to match form id=contactform

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
-// Get the message input element
-const messageInput = document.getElementById('message-input');
-
-// Add an event listener for the 'keydown' event
-messageInput.addEventListener('keydown', function(event) {
-  // Check if the pressed key is the Enter key
-  if (event.key === 'Enter') {
-    // Display a message
-    alert('You pressed the Enter key!');
-  }
+const form = document.getElementById('contactform');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  alert('Your Form Has Been Successfully Submitted');
 });
+

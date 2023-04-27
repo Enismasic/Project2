e9f9b2afa717281a02b3242ce44cdbbcdde2ace2
Enismasic <emasic@hawk.iit.edu>
2023-04-27 01:57:50 -0500

    Making fix to js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const form = document.getElementById('contactform');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  alert('Your Form Has Been Successfully Submitted');
+  alert('Thank You For Submitting');
 });
-

a9ca4adf0c41d361ce0442887c8d878c18397b66
Enismasic <emasic@hawk.iit.edu>
2023-04-27 02:10:00 -0500

    Reload page after user press enter

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const form = document.getElementById('contactform');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  alert('Thank You For Submitting');
+  if (confirm('Thank You For Submitting')) {
+    location.reload();
+  }
 });
